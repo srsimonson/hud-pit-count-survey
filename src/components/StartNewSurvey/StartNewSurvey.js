3412e9978776e5bd087864e2b6a1369406d11778
Steve Simonson <srsimonson@gmail.com>
2020-06-29 23:00:41 -0500

    StartNewSurveyComponent began

--- src/components/StartNewSurvey/StartNewSurvey.js
+++ src/components/StartNewSurvey/StartNewSurvey.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const LogOutButton = props => (
+const StartNewSurvey = props => (
   <button
     // This button shows up in multiple locations and is styled differently
     // because it's styled differently depending on where it is used, the className
@@ -9,7 +9,7 @@ const LogOutButton = props => (
     className={props.className}
     onClick={() => props.dispatch({ type: 'LOGOUT' })}
   >
-    Log Out
+    Start New Survey
   </button>
 );
 
@@ -17,4 +17,4 @@ const LogOutButton = props => (
 // because it doesn't care what the current state is.
 // No matter what the redux state is, this button will always be a log out button
 // this component still needs 'connect' though, because it is going to dispatch a redux action
-export default connect()(LogOutButton);
+export default connect()(StartNewSurvey);
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import StartNewSurvey from '../StartNewSurvey/StartNewSurvey'
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
+
 const UserPage = (props) => (
   <div>
     <h1 id="welcome">
       Welcome, { props.user.username }!
     </h1>
+    {/* <button onClick={this.startSurvey}>Start New Survey</button>
+    <button onClick={this.viewCompletedSurveys}>View Completed Surveys</button> */}
     <p>Your ID is: {props.user.id}</p>
+    <StartNewSurvey/>
     <LogOutButton className="log-in" />
   </div>
 );
