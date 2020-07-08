6ae8d7b3e1a30965b5f7d13a1aed183fc096fdc0
Steve Simonson <srsimonson@gmail.com>
2020-07-08 15:26:46 -0500

    resources page ready to incorporate PUT

--- src/components/ResourcePage/ResourcePage.js
+++ src/components/ResourcePage/ResourcePage.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import './AccordionView.scss';
-// // import bulmaCollapsible from '@creativebulma/bulma-collapsible';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import {Accordion, Card} from 'react-bootstrap';
-// import { Accordion, AccordionItem } from 'react-light-accordion'
-// import { Accordion, AccordionItem } from 'react-sanfona';
-
 
 class ResourcePage extends Component {
 
@@ -14,7 +7,7 @@ class ResourcePage extends Component {
     user_id: this.props.reduxStore.user.id,
     surveyAnswer: '', // remove
     resource_id: '', // needed? remove?
-    toggleValue: true
+    toggleValue: 0
   }
 
   componentDidMount = () => {
@@ -24,15 +17,6 @@ class ResourcePage extends Component {
     this.setState({
       user_id: this.props.reduxStore.user.id
     })
-    console.log('hi', this.props.reduxStore.resourceReducer);
-    
-  }
-
-  toggle = () => {
-    this.setState({
-      toggleValue: !this.state.toggleValue
-    })
-    console.log('toggle value:', this.state.toggleValue);
   }
 
   // handleChange = (event) => {
@@ -49,22 +33,96 @@ class ResourcePage extends Component {
   //   });
   // }
 
+  // toggle = () => {
+  //   this.setState({
+  //     toggleValue: !this.state.toggleValue
+  //   })
+  //   console.log('toggle:', this.state.toggleValue);
+    
+  // }
+
   render() {
-    // console.log('asdf this.state', this.state);
-    // console.log('this.props.reduxStore:', this.props.reduxStore);
     return (
       <div>
         <h1>Resources</h1>
-        <ul>
+        {/* <ul>
           {this.props.reduxStore.resourceReducer.map(item =>
           <li key={item.id}>{item.resource_target} {item.resource_name} {item.resource_phone} {item.resource_location}        
           <button className="button" onClick={this.toggle}>UPDATE</button></li>)}
+        </ul> */}
+
+        <ul> {/* Display List of Info */}
+          {this.props.reduxStore.resourceReducer.map(item =>
+            <li key={item.id}>
+              {
+                this.state.toggleValue !== item.resource_id ? 
+                  <div>
+                    <h2>{item.resource_name}</h2> 
+                    <p>{item.resource_phone}</p> 
+                    <p>{item.resource_location}</p>
+                    <p>Population Serves: {item.resource_target}</p>
+                    <button className="button" onClick={ () => {       
+                      this.setState({ toggleValue: item.resource_id }) 
+                      console.log('item.id:', item.resource_id);
+                    }}>UPDATE</button> 
+                  </div>
+                :
+                  <div>
+                    <p>Agency: <input placeholder={item.resource_name}></input></p>
+                    <p>Phone: <input placeholder={item.resource_phone}></input></p>
+                    <p>Address: <input placeholder={item.resource_location}></input></p>
+                    <p>Population Served: <input placeholder={item.resource_target}></input></p>
+                    <button className="button" onClick={ () => {       
+                      this.setState({ toggleValue: 0 }) 
+                      console.log('item.id:', item.resource_id);
+                    }}>SAVE</button> 
+                  </div>
+              }         
+            </li>)}
         </ul>
-        {this.state.toggleValue === true ? <h1>true</h1> : <h1>false</h1>}
 
 
 
-        <button className="button">SAVE</button>
+
+
+
+
+
+          {/* THIS IS ALL GOOD */}
+
+
+        {/* {this.state.toggleValue ? 
+        <ul> {/* Display List of Info 
+          {this.props.reduxStore.resourceReducer.map(item =>
+            <li key={item.id}>
+
+              {item.resource_target} {item.resource_name} {item.resource_phone} {item.resource_location}
+                      
+            <button className="button" 
+            onClick={ () => {      
+              if (item.resource_name === item.resource_name) {
+                console.log(`this resource is: ${item.resource_name}`);
+                
+              } 
+                this.setState({
+                  toggleValue: !this.state.toggleValue
+            })
+          }}>UPDATE</button></li>)}
+        </ul> 
+        : 
+        <ul> {/* Edit List of Info 
+          {this.props.reduxStore.resourceReducer.map(item =>
+          <li key={item.id}> <input placeholder={item.resource_target}></input> 
+            <input placeholder={item.resource_name}></input> <input placeholder={item.resource_phone}></input> <input placeholder={item.resource_location}></input>        
+          <button className="button" 
+            onClick={ () => {       
+            // console.log('this resource is:', item.resource_name);
+            this.setState({
+              toggleValue: !this.state.toggleValue
+        })
+          }}>SAVE</button></li>)}
+        </ul>} */}
+        {/* <button className="button">SAVE</button> */}
       </div>
     );
   }
