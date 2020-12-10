e3d177dc550caa3130c7ac0bb6921fe47461f31d
Steve Simonson <srsimonson@gmail.com>
2020-12-09 21:24:56 -0600

    refactoring

--- server/routes/survey.router.js
+++ server/routes/survey.router.js
@@ -46,41 +46,29 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     
     let sqlText = `INSERT INTO survey (user_id, survey_q1, survey_q2, survey_q3, survey_q4, survey_q5, survey_q6, survey_q7, survey_q8, survey_q9, survey_q10, survey_q11, survey_q12, survey_q13, survey_q14, survey_q15, survey_q16, survey_q17, survey_q18, survey_q19, survey_q20, survey_q21, survey_q22, survey_q23, survey_q24, survey_q25, survey_q26, survey_q27, survey_q28, survey_q29, survey_q30, survey_q31, survey_q32)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33);`;
-    pool.query(sqlText, 
-    [user_id, 
-    req.body.survey_q1,
-    req.body.survey_q2,
-    req.body.survey_q3,
-    req.body.survey_q4,
-    req.body.survey_q5,
-    req.body.survey_q6,
-    req.body.survey_q7,
-    req.body.survey_q8,
-    req.body.survey_q9,
-    req.body.survey_q10,
-    req.body.survey_q11,
-    req.body.survey_q12,
-    req.body.survey_q13,
-    req.body.survey_q14,
-    req.body.survey_q15,
-    req.body.survey_q16,
-    req.body.survey_q17,
-    req.body.survey_q18,
-    req.body.survey_q19,
-    req.body.survey_q20,
-    req.body.survey_q21,
-    req.body.survey_q22,
-    req.body.survey_q23,
-    req.body.survey_q24,
-    req.body.survey_q25,
-    req.body.survey_q26,
-    req.body.survey_q27,
-    req.body.survey_q28,
-    req.body.survey_q29,
-    req.body.survey_q30,
-    req.body.survey_q31,
-    req.body.survey_q32,
-    ])
+    // let sqlText;
+    // let insertText;
+    // let valuesText;
+    // for (let i=1; i<34; i++) {
+    //     if (!i === 33) {
+    //         insertText = 
+    //         sqlText = `INSERT INTO survey (user_id, survey_q${i}) VALUES ($${i});`
+    //     } else {
+    //         sqlText = `INSERT INTO survey (user_id, survey_q32) VALUES ($33);`
+    //     }
+    // }
+
+    // let x
+    // for (let i=0; i<33; i++) {
+    //     x = `req.body.survey_q${i}`
+    //     let q
+    //     x = q.replace(/"/g, ``);
+    //     console.log('hi');
+    // }
+    
+
+    pool.query(sqlText, [user_id, req.body.survey_q1, req.body.survey_q2, req.body.survey_q3, req.body.survey_q4, req.body.survey_q5, req.body.survey_q6, req.body.survey_q7, req.body.survey_q8, req.body.survey_q9, req.body.survey_q10, req.body.survey_q11, req.body.survey_q12, req.body.survey_q13, req.body.survey_q14, req.body.survey_q15, req.body.survey_q16, req.body.survey_q17, req.body.survey_q18, req.body.survey_q19, req.body.survey_q20, req.body.survey_q21, req.body.survey_q22, req.body.survey_q23, req.body.survey_q24, req.body.survey_q25, req.body.survey_q26, req.body.survey_q27, req.body.survey_q28, req.body.survey_q29, req.body.survey_q30, req.body.survey_q31, req.body.survey_q32,])
+
     .then(result => {
         // res.send(result.rows);
         res.sendStatus(200)
